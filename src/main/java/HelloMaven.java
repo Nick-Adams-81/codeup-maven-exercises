aa538db21c7c0af724632e6b96d7ec56d85e2349
nick adams <thebigdeal916@icloud.com>
2021-11-12 11:49:51 -0600

    finished maven exercise

--- src/main/java/HelloMaven.java
+++ src/main/java/HelloMaven.java
@@ -5,20 +5,17 @@ import java.util.Scanner;
 public class HelloMaven {
 
     public static void main(String[] args) {
-        String hello = "Helloworld";
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter a word[s]");
         String res = sc.nextLine();
         System.out.println("You entered " + res);
+        System.out.println("is your entry numeric? " + StringUtils.isNumeric(res));
+
         res = StringUtils.swapCase(res);
         System.out.println("Your input w/ swapped case is:" + res);
         res = StringUtils.reverse(res);
         System.out.println("your input reversed is: " + res);
-
-
-        System.out.println(hello);
-
 
 
     }
